319fe537b84261cde505bd050c08035bd6c6c822
pfeierabend <147249820+pfeierabend@users.noreply.github.com>
2023-10-07 16:50:37 +0200

    Update feature-toggles.data.ts

--- src/app/feature-toggle/feature-toggles.data.ts
+++ src/app/feature-toggle/feature-toggles.data.ts
@@ -1,13 +1,13 @@
 /* eslint-disable */
 export const FEATURE_TOGGLES = {
   downloadable_pdf: false,
-  tab_publications: true,
+  tab_publications: false,
   tab_projects: true,
-  tab_volunteering: true,
+  tab_volunteering: false,
   contact_show_about_section: true,
-  contact_email_link: false,
+  contact_email_link: true,
   contact_email_remove_at_sign: false,
-  default_language: 'EN', // please make sure your language is added to ../l10n/l10n.data.ts
+  default_language: 'DE', // please make sure your language is added to ../l10n/l10n.data.ts
   localization: true,
   cvitem_show_position_count_badge: false,
 };
